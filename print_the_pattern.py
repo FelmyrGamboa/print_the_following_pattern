fab5c85bace3374be4d35acdc6558078a555aaeb
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-23 00:09:08 +0800

    Pseudocodes

--- print_the_pattern.py
+++ print_the_pattern.py
@@ -6,3 +6,10 @@
 # 3 3 3 
 # 4 4 4 4 
 # 5 5 5 5 5
+
+#Create a for loop for the row pattern
+
+#Create another for loop for the column of pattern
+#Display the output for the column
+
+#Display the output for the row
