d8ce12f87b54723ccb5ad4088e39ca4ad09e38b1
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-23 00:17:40 +0800

    Created the nested loop for the row and column of the pattern

--- print_the_pattern.py
+++ print_the_pattern.py
@@ -8,8 +8,9 @@
 # 5 5 5 5 5
 
 #Create a for loop for the row pattern
-
+for row in range(1,6):
 #Create another for loop for the column of pattern
+    for column in range(row):
 #Display the output for the column
 
 #Display the output for the row
